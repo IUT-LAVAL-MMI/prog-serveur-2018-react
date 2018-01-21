ddf04220dbe03d7424252c0270f95a82efec8057
Martino Bettucci <martino.bettucci@maplink.global>
2018-01-21 20:03:49 +0100

    Added the Axios library dependency Added the back-end call to retrieve data

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class TodoContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: []
+        }
+    }
+    componentDidMount() {
+        axios.get('http://localhost:4000/v1/to_do.json')
+            .then(response => {
+                console.log(response)
+                this.setState({todos: response.data})
+            })
+            .catch(error => console.log(error))
+    }
     render() {
         return (
             <div>
-                Todos
+                {this.state.todos.map((todos) => {
+                    return(
+                        <div className="tile" key={todos.id} >
+                            <h4>{todos.owner}</h4>
+                            <p>{todos.content}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
